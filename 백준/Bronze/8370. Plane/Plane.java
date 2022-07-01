5bc35eb4e6319d3c8f3f58f7388c8919a3b26bfd
Vaughan <jwst0208@gmail.com>
2022-07-01 22:37:35 +0900

    [Bronze V] Title: Plane, Time: 216 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner s = new Scanner(System.in);
+    	int n1 = s.nextInt();
+    	int n2 = s.nextInt();
+    	int n3 = s.nextInt();
+    	int n4 = s.nextInt();
+    	System.out.println(n1*n2 + n3*n4);
+    }
+}
