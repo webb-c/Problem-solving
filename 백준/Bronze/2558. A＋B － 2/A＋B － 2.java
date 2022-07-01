0ddb05f0affb116178334cef6c57c3d1632fddd8
Vaughan <jwst0208@gmail.com>
2022-07-01 22:30:56 +0900

    [Bronze V] Title: A+B - 2, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		int num1 = s.nextInt();
+		s.nextLine();
+		int num2 = s.nextInt();
+		System.out.println(num1+num2);
+	}
+}
