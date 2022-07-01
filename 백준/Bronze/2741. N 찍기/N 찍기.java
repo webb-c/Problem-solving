4e66467748fd819ccf37cd7a9da47459b7d90760
Vaughan <jwst0208@gmail.com>
2022-07-01 22:31:29 +0900

    [Bronze V] Title: N 찍기, Time: 1120 ms, Memory: 33216 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main { 								 //백준 : public 없애고 Test -> Main으로 변경
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		
+		for(int i=1 ; i<=num ; i++)
+			System.out.println(i);
+	}
+}
