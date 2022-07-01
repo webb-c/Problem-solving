a8c29619b14eeefa7f39e300a6d8dc9b1fead616
Vaughan <jwst0208@gmail.com>
2022-07-01 22:37:01 +0900

    [Bronze V] Title: 등록, Time: 128 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] 등록 - 7287 
+
+[문제 링크](https://www.acmicpc.net/problem/7287) 
+
+### 성능 요약
+
+메모리: 14052 KB, 시간: 128 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+    	//Scanner s = new Scanner(System.in);
+    	System.out.println("34");
+    	System.out.println("jwst0210");
+    }
+}
