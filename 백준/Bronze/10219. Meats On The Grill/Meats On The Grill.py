e8cba5b0732ead52f8d9595894d7c9aac155aed8
Vaughan <jwst0208@gmail.com>
2023-01-25 08:31:03 +0900

    [Bronze I] Title: Meats On The Grill, Time: 80 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10219. Meats On The Grill/Meats On The Grill.py
+++ 백준/Bronze/10219. Meats On The Grill/Meats On The Grill.py
@@ -4,8 +4,6 @@ n = int(input())
 for _ in range(n) :
     # 입력
     h, w = map(int, input().split())
-    fireState = []
-    meatList = []
     for i in range(h) :
         line = input()
         print(''.join(reversed(line)))
