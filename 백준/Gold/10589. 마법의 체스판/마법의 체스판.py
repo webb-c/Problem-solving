fb4f309632efbbf1bbdf2159b131ea3e40d50c0d
Vaughan <jwst0208@gmail.com>
2023-02-06 23:04:40 +0900

    Update 마법의 체스판.py
    쓸데없는 코드 삭제

--- 백준/Gold/10589. 마법의 체스판/마법의 체스판.py
+++ 백준/Gold/10589. 마법의 체스판/마법의 체스판.py
@@ -1,12 +1,6 @@
 n, m = map(int, input().split())
-neven = False
-meven = False
-
-if n%2 == 0 : neven = True
-if m%2 == 0 : meven = True
 k = n//2 + m//2
 print(k)
-
 for i in range(2, m+1, 2) :
     print(1, i, n, i)
 for j in range(2, n+1, 2) :
