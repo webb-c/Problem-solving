996fb257aaa5915a1458ce84c75e4e122793ab5e
Vaughan <jwst0208@gmail.com>
2023-03-19 13:47:28 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+result = []
+def backTracking(indexA):
+    # 출력 후 백트래킹
+    if len(result) == M :
+        print(' '.join(map(str, result)))
+        return
+    # DFS
+    for i in range(indexA, N+1) :
+        result.append(i)
+        backTracking(i)
+        result.pop()
+backTracking(1)
