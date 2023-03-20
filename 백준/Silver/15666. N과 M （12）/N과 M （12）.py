c7c1471c8943bfd16c1a8e85713cc6eed5798a45
Vaughan <jwst0208@gmail.com>
2023-03-20 18:30:12 +0900

    [Silver II] Title: N과 M (12), Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nList = list(map(int, input().split()))
+nList.sort()
+visited = [False]*N
+result = []
+
+def backTracking(index):
+    # 출력 후 백트래킹
+    if len(result) == M :
+        print(' '.join(map(str, result)))
+        return
+    # DFS
+    check = 0
+    for i in range(index, N) :
+        n = nList[i]
+        if check == n : continue
+        result.append(n)
+        check = nList[i]
+        backTracking(i)
+        result.pop()
+backTracking(0)
