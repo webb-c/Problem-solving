4dcbbe918ab69668230ba7266599150b9348c8b0
Vaughan <jwst0208@gmail.com>
2023-08-18 22:26:55 +0900

    [Bronze V] Title: A+B - 8, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for i in range(1, T+1) :
+    A, B = map(int, input().split())
+    print("Case #"+str(i)+": "+str(A)+" + "+str(B)+" = "+str(A+B))
+    
