921794f59722ea09cd1711f8436ad49619b6e587
Vaughan <jwst0208@gmail.com>
2023-08-25 17:26:32 +0900

    [Silver V] Title: 최소공배수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,22 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def get_gcd(a, b):
+    while True:
+        if b == 0:
+            return a
+        r = a % b
+        a, b = b, r
+
+def get_lcm(a, b):
+    gcd = get_gcd(a, b)
+    return a*b // gcd
+
+def main():
+    A, B = map(int, input().split())
+    print(get_lcm(A, B))
+
+if __name__ == "__main__":
+    main()
