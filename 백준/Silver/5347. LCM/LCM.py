643ee1b91f56a5cc95f2570717a7966aaa95ed43
Vaughan <jwst0208@gmail.com>
2023-09-08 10:27:28 +0900

    [Silver V] Title: LCM, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.py
@@ -0,0 +1,24 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def get_gcd(a, b):
+    while True:
+        if b == 0:
+            return a
+        r = a % b
+        a, b = b, r
+
+def get_lcm(a, b):
+    gcd = get_gcd(a, b)
+    return a*b // gcd
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        a, b = map(int, input().split())
+        print(get_lcm(a, b))
+
+if __name__ == "__main__":
+    main()
