29a4f3ee6bda52d9bbd66cb171a94ddc6a73b0d7
Vaughan <jwst0208@gmail.com>
2024-07-30 15:37:08 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,35 +1,27 @@
 import sys
-import math
-from random import randrange
-# import statistics
-# from itertools import combinations
-# from fractions import Fraction
-# import heapq
-# from collections import Counter
-# from collections import deque
-# import operator as op
-# from functools import reduce
 
 sys.setrecursionlimit(10**6)
-
 MOD = 1000000000
 
-def get_step(N, DP):
-    for n in range(2, N+1):
-        for i in range(10):
-            if i == 0 :
-                DP[n][i+1] += DP[n-1][i]
-                DP[n][i+1] = DP[n][i+1] % MOD
-            elif i == 9:
-                DP[n][i-1] += DP[n-1][i]
-                DP[n][i-1] = DP[n][i-1] % MOD
-            else:
-                DP[n][i+1] += DP[n-1][i]
-                DP[n][i-1] += DP[n-1][i]
-                DP[n][i+1], DP[n][i-1] = DP[n][i+1] % MOD, DP[n][i-1] % MOD
-            
-    return sum(DP[N]) % 1000000000
+def get_step(N):
+    # DP 배열 대신 두 행만 유지 (메모리 개선)
+    prev_dp = [0] * 10
+    curr_dp = [0] * 10
     
+    for i in range(1, 10):
+        prev_dp[i] = 1
+    
+    for n in range(2, N + 1):
+        curr_dp = [0] * 10  
+        for i in range(10):
+            if i > 0:
+                curr_dp[i - 1] = (curr_dp[i - 1] + prev_dp[i]) % MOD
+            if i < 9:
+                curr_dp[i + 1] = (curr_dp[i + 1] + prev_dp[i]) % MOD
+        prev_dp = curr_dp  
+    
+    return sum(prev_dp) % MOD
+
 
 def input():
     return sys.stdin.readline().rstrip()
@@ -37,11 +29,11 @@ def input():
 
 def solve():
     N = int(input())
-    DP = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0] for _ in range(N+1)]
-    DP[1] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    print(get_step(N, DP))
+    if N == 1:
+        print(9)  # 길이가 1인 경우 계단 수는 1~9까지 9개
+    else:
+        print(get_step(N))
     
     
 if __name__ == "__main__":
     solve()
-    
