9a5bd31ff9a2b89b0998e731bd901d2b739254db
Vaughan <jwst0208@gmail.com>
2024-08-01 14:46:10 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 48 ms, Memory: 33184 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,17 +1,17 @@
 import sys
+from bisect import bisect_left
 
-def get_longest_increasing_subsequence_length_via_dp(N, A):
-    DP = [1] * N
+def get_longest_increasing_subsequence_length_via_binary_search(N, A):
+    LIS = [A[0]]
     
     for i in range(1, N):
         entry = A[i]
-        length = 0
-        for j in range(i):
-            if A[j] < entry: length = max(length, DP[j])
-        
-        DP[i] = length + 1
-    
-    return max(DP)
+        if entry > LIS[-1]: 
+            LIS.append(entry)
+        else:
+            LIS[bisect_left(LIS, entry)] = entry
+            
+    return len(LIS)
 
 
 def input():
@@ -22,7 +22,7 @@ def solve():
     N = int(input())
     A = list(map(int, input().split()))
     
-    print(get_longest_increasing_subsequence_length_via_dp(N, A))
+    print(get_longest_increasing_subsequence_length_via_binary_search(N, A))
     
 
 if __name__ == "__main__":
